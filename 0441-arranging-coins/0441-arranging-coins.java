c0e4cd5be68d6c1a787170eed3a1dc4e51a1144f
Rajesh Shewale <shewalerajesh22@gmail.com>
2026-08-01 18:24:39 +0530

    Time: 1 ms (100%), Space: 42.7 MB (44.63%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        long start = 1;
+        long end = n;
+        while( start <= end ){
+            long mid = start + ( end - start )/2;
+            long  isrequiredc = mid*( mid + 1) / 2;
+            if( isrequiredc <= n){
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        return (int) end;
+    }
+}
