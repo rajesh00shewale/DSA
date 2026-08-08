9fee4c6297db2147c1a2f3301d421be6c5185bc8
Rajesh Shewale <shewalerajesh22@gmail.com>
2026-08-08 21:33:01 +0530

    Time: 5 ms (81.23%), Space: 45.8 MB (72.66%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+
+        int reverse = 0,
+            copy = x;
+        while (x > 0) {
+            reverse = (reverse * 10) + (x % 10);
+            x /= 10;
+        }
+        return reverse == copy;
+    }
+}
